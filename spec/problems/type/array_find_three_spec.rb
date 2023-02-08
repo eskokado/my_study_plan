b2cad343f3fc854952030a4996713fe504474fb8
eskokado <eskokado@gmail.com>
2023-02-08 07:03:59 -0300

    test: Given array size correct return message

--- spec/problems/type/array_find_three_spec.rb
+++ spec/problems/type/array_find_three_spec.rb
@@ -7,4 +7,10 @@ describe "Print 3 largest" do
     result = printLargest.print3largest(arr)
     expect(result).to eq("Invalid Input")
   end
+  it "Given array size correct return message" do
+    arr = [12, 13, 1, 10, 34, 1]
+    printLargest = PrintLargest.new
+    result = printLargest.print3largest(arr)
+    expect(result).to eq("Three largest elements are 34 13 12")
+  end
 end

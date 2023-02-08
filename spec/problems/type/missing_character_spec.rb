480e6f66b8ff8804ae568c32550b66063e22d6b0
eskokado <eskokado@gmail.com>
2023-02-08 06:01:00 -0300

    test: should call missing characters 'The quick brown fox jumps' result 'adglvyz'

--- spec/problems/type/missing_character_spec.rb
+++ spec/problems/type/missing_character_spec.rb
@@ -15,4 +15,11 @@ describe "Missing Characters" do
 
     expect(result).to eq("abdhijnpquvxyz")
   end
+  it "should call missing characters 'The quick brown fox jumps' result 'adglvyz'" do
+    str = "The quick brown fox jumps"
+    gfg = GFG.new
+    result = gfg.missing_chars(str)
+
+    expect(result).to eq("adglvyz")
+  end
 end

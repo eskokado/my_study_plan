0a82cc20429d2a26bc829956a923973626216e0f
eskokado <eskokado@gmail.com>
2023-02-09 07:16:49 -0300

    fix: Create class KruscalsMST with method kruskals

--- app/algorithm/type/minimum_spanning.rb
+++ app/algorithm/type/minimum_spanning.rb
@@ -1,69 +1,55 @@
-class Graph
+class KruskalsMST
   attr_accessor :V, :E, :edge
 
-  def initialize(v, e)
-    @V = v
-    @E = e
-    @edge = []
-    for i in 0..e - 1
-      @edge.push(Edge.new)
+  def findRoot(subsets, i)
+    if (subsets[i].parent == i)
+      return subsets[i].parent
     end
+    subsets[i].parent = findRoot(subsets, subsets[i].parent)
+    return subsets[i].parent
   end
 
-  def find(subsets, x)
-    unless subsets[x].parent == x
-      subsets[x].parent = find(subsets, subsets[x].parent)
-    end
-    subsets[x].parent
-  end
+  def union(subsets, x, y)
+    rootX = findRoot(subsets, x)
+    rootY = findRoot(subsets, y)
 
-  def Union(subsets, x, y)
-    xroot = find(subsets, x)
-    yroot = find(subsets, y)
-
-    if subsets[xroot].rank < subsets[yroot].rank
-      subsets[xroot].parent = yroot
-    elsif subsets[yroot].rank < subsets[xroot].rank
-      subsets[yroot].parent = xroot
+    if subsets[rootY].rank < subsets[rootX].rank
+      subsets[rootY].parent = rootX
+    elsif subsets[rootX].rank < subsets[rootY].rank
+      subsets[rootX].parent = rootY
     else
-      subsets[yroot].parent = xroot
-      subsets[xroot].rank += 1
+      subsets[rootY].parent = rootX
+      subsets[rootX].rank += 1
     end
   end
 
-  def KruskalMST
-    result = []
-    for i in 0..(@V - 1)
-      result.push(Edge.new)
-    end
-    @edge.sort_by! {|e| e.weight}
+  def kruskals(v, edges)
+    j = 0
+    noOfEdges = 0
 
     subsets = []
-    for i in 0..(@V - 1)
-      subsets.push(Subset.new)
-      subsets[i].parent = i
-      subsets[i].rank = 0
+    for i in 0..(v - 1)
+      subsets.push(Subset.new(i, 0))
     end
-    i = 0
-    e = 0
-    while e < (@V - 1) do
-      next_edge = Edge.new
-      next_edge = edge[i]
-      i += 1
 
-      x = find(subsets, next_edge.src)
-      y = find(subsets, next_edge.dest)
+    result = []
+    while noOfEdges < (v - 1) do
+      nextEdge = edges[j]
+      x = findRoot(subsets, nextEdge.src)
+      y = findRoot(subsets, nextEdge.dest)
 
-      if x != y
-        result[e] = next_edge
-        e += 1
-        Union(subsets, x, y)
+      if (x != y)
+        result.push(nextEdge)
+        union(subsets, x, y)
+        noOfEdges += 1
       end
+
+      j += 1
     end
 
     textResult = "Following are the edges in the constructed MST\n"
     minimumCost = 0
-    for i in 0.. e - 1
+    for i in 0..noOfEdges - 1
       textResult += "#{result[i].src} -- #{result[i].dest} == #{result[i].weight}\n"
       minimumCost += result[i].weight
     end
@@ -71,13 +57,23 @@ class Graph
 
     return textResult
   end
-
 end
 
 class Edge
   attr_accessor :src, :dest, :weight
+
+  def initialize(src, dest, weight)
+    @src = src
+    @dest = dest
+    @weight = weight
+  end
 end
 
 class Subset
   attr_accessor :parent, :rank
+
+  def initialize(parent, rank)
+    @parent = parent
+    @rank = rank
+  end
 end

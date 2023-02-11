dd67bd0740d7fe75f3a3365a4b26d41b1f593c18
eskokado <eskokado@gmail.com>
2023-02-11 10:10:37 -0300

    feat: create algorithm jump search

---
+++ app/algorithm/type/jump_search.rb
@@ -0,0 +1,20 @@
+class JumpSearch
+  def search(arr, n, item)
+    i = 0
+    m = Math.sqrt(n).round()
+    while arr[m] <= item && m < n do
+      i = m
+      m += Math.sqrt(n).round()
+      if m > (n - 1)
+        return -1
+      end
+    end
+
+    for x in i..(m-1) do
+      if (arr[x] == item)
+        return x
+      end
+    end
+    return -1
+  end
+end

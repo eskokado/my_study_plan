694e7e494e467dd8f1ea0969887dab460fe41842
eskokado <eskokado@gmail.com>
2023-02-12 05:23:26 -0300

    refactor: organizing files into folders by types of algorithms

--- spec/algorithms/searching/jump_search_spec.rb
+++ spec/algorithms/searching/jump_search_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../../app/algorithm/type/jump_search'
+require_relative '../../../app/algorithms/searching/jump_search'
 describe "Jump Search" do
   arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 55, 77, 89, 101, 201, 256, 780]
   expectValue = 10

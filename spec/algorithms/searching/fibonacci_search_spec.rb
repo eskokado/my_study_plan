295f5843ca5930db0dbe4220ed6d397fd0aa6415
eskokado <eskokado@gmail.com>
2023-02-12 08:36:07 -0300

    test: Given an array call fibonacci search return index of element

---
+++ spec/algorithms/searching/fibonacci_search_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../../app/algorithms/searching/fibonacci_search'
+describe "Fibonacci search" do
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  it "Given an array call fibonacci search return index of element" do
+    item = 8
+    result = Algorithms::Searching.fibonacci_search(array, item)
+    expect(result).to be > -1
+  end
+end

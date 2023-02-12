47cc678af0f0b20f2cc717221fe90da0b59f34a3
eskokado <eskokado@gmail.com>
2023-02-12 05:07:01 -0300

    refactor: organizing files into folders by types of algorithms

--- spec/algorithms/sorting/counting_sort_spec.rb
+++ spec/algorithms/sorting/counting_sort_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../../app/algorithm/type/counting_sort'
+require_relative '../../../app/algorithms/sorting/counting_sort'
 
 describe "Counting Sort" do
   countingSort = CountingSort.new

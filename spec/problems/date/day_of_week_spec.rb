9a96b46452ee30c38bfc4013f947a2b18df056e9
eskokado <eskokado@gmail.com>
2023-02-16 06:19:20 -0300

    fix: returns 1 for Monday

--- spec/problems/date/day_of_week_spec.rb
+++ spec/problems/date/day_of_week_spec.rb
@@ -1,6 +1,11 @@
 require_relative '../../../app/problems/date/day_of_week'
 describe "Day of Week" do
   it "returns 0 for Sunday" do
-    expect(Problems::Date.date_of_week(2023, 2, 19)).to eq(0)
+    expect(Problems::Date.day_of_week(2023, 2, 19)).to eq(0)
   end
+
+  it "returns 1 for Monday" do
+    expect(Problems::Date.day_of_week(2023, 2, 20)).to eq(1)
+  end
+
 end

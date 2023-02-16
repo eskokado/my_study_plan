492d99e160dd37d400a6efff72efeb999ce348a5
eskokado <eskokado@gmail.com>
2023-02-16 05:51:56 -0300

    test: returns the correct time when adding minutes to 23:59

--- spec/problems/date/sum_minutes_spec.rb
+++ spec/problems/date/sum_minutes_spec.rb
@@ -6,4 +6,10 @@ describe 'Sum Minute' do
     expect(Problems::Date.sum_minutes('10:00', 1440)).to eq ('10:00')
     expect(Problems::Date.sum_minutes('10:00', 1500)).to eq ('11:00')
   end
+
+  it 'returns the correct time when adding minutes to 23:59' do
+    expect(Problems::Date.sum_minutes('23:59', 1)).to eq('00:00') # rolagem de hora
+    expect(Problems::Date.sum_minutes('23:59', 60)).to eq('00:59') # rolagem de hora
+    expect(Problems::Date.sum_minutes('23:59', 1440)).to eq('23:59') # 24 horas
+  end
 end

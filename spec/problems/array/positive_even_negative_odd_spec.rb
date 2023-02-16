e878be6f81ee4808b8e82310ab6e195db19157c3
eskokado <eskokado@gmail.com>
2023-02-16 04:49:06 -0300

    test: rearranges the array correctly for an array with only positive numbers

--- spec/problems/array/positive_even_negative_odd_spec.rb
+++ spec/problems/array/positive_even_negative_odd_spec.rb
@@ -7,5 +7,12 @@ describe Problems::Arrays do
       result = Problems::Arrays.positive_even_negative_odd(arr)
       expect(result).to eq([2, -3, 5, -7, 6, -8])
     end
+
+    it "rearranges the array correctly for an array with only positive numbers" do
+      arr = [1, 2, 3, 4, 5, 6]
+      result = Problems::Arrays.positive_even_negative_odd(arr)
+      expect(result).to eq([1, nil, 2, nil, 3, nil, 4, nil, 5, nil, 6])
+    end
+
   end
 end

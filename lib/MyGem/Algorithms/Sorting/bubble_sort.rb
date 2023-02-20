075c6d08c188685f4722094dc97e53522abdba97
eskokado <eskokado@gmail.com>
2023-02-20 05:47:39 -0300

    feat: create gem bubble sort in my_gem

---
+++ lib/MyGem/Algorithms/Sorting/bubble_sort.rb
@@ -0,0 +1,21 @@
+module MyGem
+  module Algorithms
+    module Sorting
+      class BubbleSort
+        def self.sort(arr)
+          n = arr.size()
+          for i in 0..(n - 2)
+            for j in 0..(n - i - 2)
+              if arr[j] > arr[j + 1]
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+              end
+            end
+          end
+          return arr
+        end
+      end
+    end
+  end
+end

648cfcd11b910b5bd1df42295420ffbc28159097
eskokado <eskokado@gmail.com>
2023-02-21 04:23:13 -0300

    test: should call missing characters 'The quick brown fox jumps' result 'adglvyz'

--- spec/MyGem/Problems/Strings/missing_character_spec.rb
+++ spec/MyGem/Problems/Strings/missing_character_spec.rb
@@ -13,4 +13,10 @@ describe "Missing Characters" do
 
     expect(result).to eq("abdhijnpquvxyz")
   end
+  it "should call missing characters 'The quick brown fox jumps' result 'adglvyz'" do
+    str = "The quick brown fox jumps"
+    result = MyGem::Problems::Strings::GFG.missing_chars(str)
+
+    expect(result).to eq("adglvyz")
+  end
 end

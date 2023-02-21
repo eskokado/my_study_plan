1692f4840e6989b205155db9f88368886b1eee67
eskokado <eskokado@gmail.com>
2023-02-21 05:23:15 -0300

    test: when given a string with only uppercase letters returns the correct count for uppercase letters

--- spec/MyGem/Problems/Strings/counting_string_spec.rb
+++ spec/MyGem/Problems/Strings/counting_string_spec.rb
@@ -13,5 +13,16 @@ describe MyGem::Problems::Strings::CountingString do
       end
     end
 
+    context "when given a string with only uppercase letters" do
+      it "returns the correct count for uppercase letters" do
+        counting_string = MyGem::Problems::Strings::CountingString.new
+        counting_string.count("ABCDEF")
+        expect(counting_string.countUpper).to eq(6)
+        expect(counting_string.countLower).to eq(0)
+        expect(counting_string.countNumber).to eq(0)
+        expect(counting_string.countSpecial).to eq(0)
+      end
+    end
+
   end
 end

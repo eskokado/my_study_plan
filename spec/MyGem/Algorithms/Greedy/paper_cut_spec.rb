710f20f527c0eb921484987471ddda873224093a
eskokado <eskokado@gmail.com>
2023-02-23 08:09:02 -0300

    test: PaperCut #prim_mst returns the minimum spanning tree weight for a graph with 5 vertices and 7 edges

--- spec/MyGem/Algorithms/Greedy/paper_cut_spec.rb
+++ spec/MyGem/Algorithms/Greedy/paper_cut_spec.rb
@@ -9,5 +9,17 @@ RSpec.describe MyGem::Algorithms::Greedy::PaperCut do
       expect(graph.prim_mst).to eq(3)
     end
 
+    it 'returns the minimum spanning tree weight for a graph with 5 vertices and 7 edges' do
+      graph = MyGem::Algorithms::Greedy::PaperCut.new(5)
+      graph.add_edge(0, 1, 2)
+      graph.add_edge(0, 2, 3)
+      graph.add_edge(1, 2, 1)
+      graph.add_edge(1, 3, 4)
+      graph.add_edge(1, 4, 5)
+      graph.add_edge(2, 4, 6)
+      graph.add_edge(3, 4, 7)
+      expect(graph.prim_mst).to eq(12)
+    end
+
   end
 end

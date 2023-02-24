2e492d5964b70ed6e2c04011d152b47664e88c85
eskokado <eskokado@gmail.com>
2023-02-24 06:11:37 -0300

    test: Reorder Index returns the indexes of an array in ascending order

---
+++ spec/MyGem/Problems/Arrays/reorder_index_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../../../../lib/MyGem/Problems/Arrays/reorder_index'
+describe "Reorder Index" do
+  it "returns the indexes of an array in ascending order" do
+    arr = [5, 2, 8, 4, 9]
+    expect(MyGem::Problems::Arrays::ReorderIndex.handle(arr, :asc)).to eq([1, 3, 0, 2, 4])
+  end
+
+end

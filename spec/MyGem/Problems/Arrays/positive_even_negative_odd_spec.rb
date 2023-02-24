f606fc9a07609282c832eed8ce6c8b5aaf30f57d
eskokado <eskokado@gmail.com>
2023-02-24 06:33:02 -0300

    test: PositiveEvenNegativeOdd rearranges the array correctly for an array with only positive numbers

--- spec/MyGem/Problems/Arrays/positive_even_negative_odd_spec.rb
+++ spec/MyGem/Problems/Arrays/positive_even_negative_odd_spec.rb
@@ -8,5 +8,11 @@ describe MyGem::Problems::Arrays::PositiveEvenNegativeOdd do
       expect(result).to eq([2, -3, 5, -7, 6, -8])
     end
 
+    it "rearranges the array correctly for an array with only positive numbers" do
+      arr = [1, 2, 3, 4, 5, 6]
+      result = MyGem::Problems::Arrays::PositiveEvenNegativeOdd.handle(arr)
+      expect(result).to eq([1, nil, 2, nil, 3, nil, 4, nil, 5, nil, 6])
+    end
+
   end
 end

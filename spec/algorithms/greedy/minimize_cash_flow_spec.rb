43f4de559125f5f2054647d75042c6ae36b8d0e1
eskokado <eskokado@gmail.com>
2023-02-26 15:44:45 -0300

    test: minimize_cash_flow_spec when there are no debts return an empty list

--- spec/algorithms/greedy/minimize_cash_flow_spec.rb
+++ spec/algorithms/greedy/minimize_cash_flow_spec.rb
@@ -7,32 +7,5 @@ RSpec.describe 'Algorithms::Greedy' do
       end
     end
 
-    context 'when there is only one debt' do
-      it 'returns a list with one transaction' do
-        debts = [['Alice', 'Bob', 10]]
-        result = Algorithms::Greedy.minimize_cash_flow(debts)
-        expect(result).to eq([['Alice', 'Bob', 10]])
-      end
-    end
-
-    context 'when there are multiple debts involving multiple people' do
-      it 'correctly balances the debts' do
-        debts = [
-          ['Alice', 'Bob', 10],
-          ['Bob', 'Charlie', 20],
-          ['Charlie', 'Alice', 15],
-          ['David', 'Charlie', 25],
-          ['David', 'Bob', 5],
-          ['Charlie', 'David', 15],
-          ['Alice', 'David', 5]
-        ]
-        result = Algorithms::Greedy.minimize_cash_flow(debts)
-        expect(result).to match_array([
-                                        ['Alice', 'Charlie', 5],
-                                        ['David', 'Charlie', 15],
-                                        ['David', 'Bob', 5]
-                                      ])
-      end
-    end
   end
 end
